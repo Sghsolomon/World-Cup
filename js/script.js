98c00bcfe9ef085258a6a77c69649f4db667463e
Sghsolomon <solomonghebrelibanos@gmail.com>
2024-03-29 22:49:47 -0700

    Js file updated latest

--- js/script.js
+++ js/script.js
@@ -22,17 +22,17 @@ async function buildTable() {
     const response = await fetch(url, options);
     const result = await response.json();
     const table = document.querySelector('#myTable');
-    console.log(result[0]);
+    console.log(result);
     for (let element of result) {
       let row = `<tr>
-      <td>${element['event_year']}</td>
-      <td>${element['event_locaiton']}</td>
-      <td>${element['event_venue']}</td>
-      <td>${element['losing_team']} (${element['final_score'].charAt(4)}) - ${
+        <td>${element['event_year']}</td>
+        <td>${element['event_locaiton']}</td>
+        <td>${element['event_venue']}</td>
+        <td>${element['losing_team']} (${element['final_score'].charAt(4)}) - ${
         element['winning_team']
       } (${element['final_score'].charAt(0)})</td>
-      <td>${element['winning_team']}</td>
-      </tr>`;
+        <td>${element['winning_team']}</td>
+        </tr>`;
       table.innerHTML += row;
     }
   } catch (error) {
