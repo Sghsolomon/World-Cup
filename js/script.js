d9232f4d8cfe2c8f60589d948218dcaf0694befa
Solomon <solomonghebrelibanos@gmail.com>
2024-04-01 15:07:57 -0700

    code cloned from my branch to revert some codes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-let roundSelector = document.querySelector('#roundDropdown');
+// let roundSelector = document.querySelector('#roundDropdown');
 
-roundSelector.addEventListener('click', showSection);
+// roundSelector.addEventListener('click', showSection);
 
-function showSection(e) {
-  let round = e.target.id;
-  let table = document.querySelector(`#${round}Table`);
-  table.style.display = 'block';
-}
+// function showSection(e) {
+//   let round = e.target.id;
+//   let table = document.querySelector(`#${round}Table`);
+//   table.style.display = 'block';
+// }
 
 async function buildTable() {
   const url = 'https://fifa-data.p.rapidapi.com/FifaWorldCupFinals';
@@ -22,15 +22,15 @@ async function buildTable() {
     const response = await fetch(url, options);
     const result = await response.json();
     const table = document.querySelector('#myTable');
-    console.log(result[0]);
+    console.log(result);
     for (let element of result) {
       let row = `<tr>
       <td>${element['event_year']}</td>
       <td>${element['event_locaiton']}</td>
       <td>${element['event_venue']}</td>
-      <td>${element['losing_team']} (${element['final_score'].charAt(4)}) - ${
+      <td>${element['losing_team']} ${element['final_score'].charAt(4)} - ${
         element['winning_team']
-      } (${element['final_score'].charAt(0)})</td>
+      } ${element['final_score'].charAt(0)}</td>
       <td>${element['winning_team']}</td>
       </tr>`;
       table.innerHTML += row;
@@ -39,3 +39,5 @@ async function buildTable() {
     console.error(error);
   }
 }
+
+buildTable();
